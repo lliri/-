942ff690a84d341990e798b06d05946610a4d89c
lliri <74451983+lliri@users.noreply.github.com>
2022-04-23 17:55:55 +0700

    Update telegram.py

--- bot telegram/telegram.py
+++ bot telegram/telegram.py
@@ -5,13 +5,13 @@ import random
 bot = telebot.TeleBot('5375856364:AAFmin7Nwl4fo8_-xzzsygzAayfoIgE1DCk')
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == "Привет":
+    if message.text == 'Расскажи анекдот':
+        x=teleani.givc()
+        bot.send_message(message.from_user.id, x)
+    elif message.text == "Привет":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "Напиши привет или напиши расскажи аникдот")
     else:
         bot.send_message(message.from_user.id, "напиши /help")
-    if message.text == 'Расскажи анекдот':
-        x=teleani.givc()
-        bot.send_message(message.from_user.id, x)
 bot.polling(none_stop=True, interval=0)
