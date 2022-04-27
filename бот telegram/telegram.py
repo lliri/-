776bcd959a6ee11a98f511ee415bb8d51901d94a
lliri <74451983+lliri@users.noreply.github.com>
2022-04-27 14:07:31 +0700

    Update and rename bot telegram/telegram.py to бот telegram/telegram.py

--- бот telegram/telegram.py
+++ бот telegram/telegram.py
@@ -9,9 +9,9 @@ def get_text_messages(message):
         x=teleani.givc()
         bot.send_message(message.from_user.id, x)
     elif message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+        bot.send_message(message.from_user.id, "Привет, я пока мало, что умею, но мой ассортимент будет увеличиваться, и чтобы узнать что я могу на данный момент напиши /help")
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет или напиши расскажи аникдот")
+        bot.send_message(message.from_user.id, "Напиши привет или напиши расскажи анекдот")
     else:
         bot.send_message(message.from_user.id, "напиши /help")
 bot.polling(none_stop=True, interval=0)
